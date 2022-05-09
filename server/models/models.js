f82397e57556f2c40b9f81000a091531366ea688
Eugene Rak <eugenemrak@gmail.com>
2022-05-09 20:59:17 +0300

    Refactoring my project

---
+++ server/models/models.js
@@ -0,0 +1,66 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+const {BelongsTo} = require("sequelize/types");
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true, },
+    password: {type: DataTypes.STRING, unique: false,},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'},
+})
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const BasketProduct = sequelize.define('basket_product', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Product = sequelize.define('product', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false, },
+    price: {type: DataTypes.INTEGER, allowNull: false,},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
+    img: {type: DataTypes.STRING,allowNull: false,},
+})
+
+const Type = sequelize.define('type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false,},
+})
+
+const Brand = sequelize.define('brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false,},
+})
+
+const Rating = sequelize.define('rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull: false,},
+})
+
+const ProductInfo = sequelize.define('product_info', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false,},
+    description: {type: DataTypes.STRING, allowNull: false,},
+})
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+
+
+
+
+
+Product.hasMany(ProductInfo)
+Product.hasMany(Rating)
+Type.hasMany(Product)
+Brand.hasMany(Product)
+Basket.hasMany(BasketProduct)
+User.hasMany(Rating)
+
+
+
+
