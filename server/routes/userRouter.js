030fbcaee7d64e9b81d6fe495c3e65c636a0d567
Eugene Rak <eugenemrak@gmail.com>
2022-05-11 21:22:18 +0300

    created frontend

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
+const authMiddleWhere = require('../middleware/authMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.get('/auth', authMiddleWhere, userController.check)
 
 module.exports = router
